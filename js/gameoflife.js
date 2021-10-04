404c192757eec64832b2d1988e2884a0d50e2735
Dominic Ngalo <otienodominic@gmail.com>
2021-10-04 13:29:17 +0300

    Completed the challenge

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -74,9 +74,18 @@ const calculateNext = (state) => {
   return new_state
 };
 
-const iterate = (state, iterations) => { };
+const iterate = (state, iterations) => {
+  const states = [state];
+  for (let i = 0; i < iterations; i++){
+    states.push(calculateNext(states[states.length - 1]));
+  }
+  return states;
+};
 
-const main = (pattern, iterations) => { };
+const main = (pattern, iterations) => {
+  const results = iterate(startPatterns[pattern], iterations);
+  results.forEach(r => console.log(printCells(r)));
+};
 
 const startPatterns = {
   rpentomino: [
