dbfeaacb11a276dcae74a67842e002a271ecbc70
Dominic Ngalo <otienodominic@gmail.com>
2021-10-04 12:50:29 +0300

    Added solution upto challenge 10

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,115 +1,131 @@
-function seed() {return [...arguments]}
+function seed() { return [...arguments] }
 
-function same([x, y], [j, k]) {return x==j && y==k}
+function same([x, y], [j, k]) { return x == j && y == k }
 
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) { return this.some(a => same(a, cell))}
+function contains(cell) { return this.some(a => same(a, cell)) }
 
 const printCell = (cell, state) => {
-   return contains.call(state, cell) ? '\u25A3' : '\u25A2'
+  return contains.call(state, cell) ? '\u25A3' : '\u25A2'
 };
 
 const corners = (state = []) => {
- // create an list of first  and second rows    
- const first = state.map(e => e[0]) 
- const second = state.map(e => e[1])   
+  // create an list of first  and second rows    
+  const first = state.map(e => e[0])
+  const second = state.map(e => e[1])
 
- if(!state.length){
-     return {topRight: [0,0], bottomLeft: [0,0]}
-   }
-   else{
-       return {
-         topRight: [Math.max(...first),Math.max(...second)] ,
-         bottomLeft: [Math.min(...first),Math.min(...second)]
-       }
-   }    
-  
+  if (!state.length) {
+    return { topRight: [0, 0], bottomLeft: [0, 0] }
+  }
+  else {
+    return {
+      topRight: [Math.max(...first), Math.max(...second)],
+      bottomLeft: [Math.min(...first), Math.min(...second)]
+    }
+  }
+
 };
 
 const printCells = (state) => {
   // Destructure bottomLeft and topRight from corners function
-  const {bottomLeft, topRight} = corners(state)
+  const { bottomLeft, topRight } = corners(state)
   // Print the states using the printCell function
   let cells = '';
-  for(let y=topRight[1]; y>=bottomLeft[1]; y--){
+  for (let y = topRight[1]; y >= bottomLeft[1]; y--) {
     let row_to_print = [];
-    for(let x=bottomLeft[0]; x<=topRight[0]; x++){
-      row_to_print.push(printCell([x,y], state))
+    for (let x = bottomLeft[0]; x <= topRight[0]; x++) {
+      row_to_print.push(printCell([x, y], state))
     }
-    cells += (row_to_print.join(' ')+ '\n')
+    cells += (row_to_print.join(' ') + '\n')
   }
-return cells
-  
+  return cells
+
 };
 
 const getNeighborsOf = ([x, y]) => {
-  return [[x-1, y-1],[x-1,y],[x-1,y+1],[x+1,y-1],[x+1,y],[x+1,y+1],[x,y-1],[x,y+1]]
+  return [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1], [x + 1, y - 1], [x + 1, y], [x + 1, y + 1], [x, y - 1], [x, y + 1]]
 };
 
 const getLivingNeighbors = (cell, state) => {
   // Get neighbours
   let neighbours = getNeighborsOf(cell)
-// Get and return Live neighbours
-return neighbours.filter(a => contains.bind(state)(a))
+  // Get and return Live neighbours
+  return neighbours.filter(a => contains.bind(state)(a))
 };
 
-const willBeAlive = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  // Calculate how many living neighbours a cell has
+  let living = getLivingNeighbors(cell, state).length // number
+  let cell_status = contains.call(state, cell) // True or false
 
-const calculateNext = (state) => {};
+  return ((cell_status && living == 2) || living == 3)
+};
 
-const iterate = (state, iterations) => {};
+const calculateNext = (state) => { 
+  const {bottomLeft, topRight} = corners(state)
+  // add a step to the row or column in the next state
+  let new_state = []
+  for(let y=topRight[1]+1; y>=bottomLeft[1]-1; y--){
+    for(let x=bottomLeft[0]-1; x<=topRight[0]+1; x++){
+      new_state = new_state.concat(willBeAlive([x,y],state) ? [[x,y]]: [])
+    }    
+  }
+  return new_state
+};
 
-const main = (pattern, iterations) => {};
+const iterate = (state, iterations) => { };
+
+const main = (pattern, iterations) => { };
 
 const startPatterns = {
-    rpentomino: [
-      [3, 2],
-      [2, 3],
-      [3, 3],
-      [3, 4],
-      [4, 4]
-    ],
-    glider: [
-      [-2, -2],
-      [-1, -2],
-      [-2, -1],
-      [-1, -1],
-      [1, 1],
-      [2, 1],
-      [3, 1],
-      [3, 2],
-      [2, 3]
-    ],
-    square: [
-      [1, 1],
-      [2, 1],
-      [1, 2],
-      [2, 2]
-    ]
-  };
-  
-  const [pattern, iterations] = process.argv.slice(2);
-  const runAsScript = require.main === module;
-  
-  if (runAsScript) {
-    if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
-      main(pattern, parseInt(iterations));
-    } else {
-      console.log("Usage: node js/gameoflife.js rpentomino 50");
-    }
+  rpentomino: [
+    [3, 2],
+    [2, 3],
+    [3, 3],
+    [3, 4],
+    [4, 4]
+  ],
+  glider: [
+    [-2, -2],
+    [-1, -2],
+    [-2, -1],
+    [-1, -1],
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [3, 2],
+    [2, 3]
+  ],
+  square: [
+    [1, 1],
+    [2, 1],
+    [1, 2],
+    [2, 2]
+  ]
+};
+
+const [pattern, iterations] = process.argv.slice(2);
+const runAsScript = require.main === module;
+
+if (runAsScript) {
+  if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
+    main(pattern, parseInt(iterations));
+  } else {
+    console.log("Usage: node js/gameoflife.js rpentomino 50");
   }
-  
-  exports.seed = seed;
-  exports.same = same;
-  exports.contains = contains;
-  exports.getNeighborsOf = getNeighborsOf;
-  exports.getLivingNeighbors = getLivingNeighbors;
-  exports.willBeAlive = willBeAlive;
-  exports.corners = corners;
-  exports.calculateNext = calculateNext;
-  exports.printCell = printCell;
-  exports.printCells = printCells;
-  exports.startPatterns = startPatterns;
-  exports.iterate = iterate;
-  exports.main = main;
+}
+
+exports.seed = seed;
+exports.same = same;
+exports.contains = contains;
+exports.getNeighborsOf = getNeighborsOf;
+exports.getLivingNeighbors = getLivingNeighbors;
+exports.willBeAlive = willBeAlive;
+exports.corners = corners;
+exports.calculateNext = calculateNext;
+exports.printCell = printCell;
+exports.printCells = printCells;
+exports.startPatterns = startPatterns;
+exports.iterate = iterate;
+exports.main = main;
--- js/new.js
+++ js/new.js
@@ -1,3 +1,9 @@
-let neighbours = [1,3,5,6,7,5,9]
-neighbours.filter(a => a>5)
-console.log(neighbours)
+const { newExpression } = require("jscodeshift")
+
+let neighbours = [1,3,5,6,5,5,9]
+// neighbours.filter(a => a>5)
+// console.log(neighbours)
+// neighbours.map(a => console.log(a))
+// neighbours.forEach(a => console.log(a))
+// neighbours.some(a => console.log(a))
+console.log(neighbours.splice(1,3))
